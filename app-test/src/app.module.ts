32a186de9cd2c34f72fd9c3fdcf2b295ca965d3b
NathanMuniz <64488928+NathanMuniz@users.noreply.github.com>
2024-03-08 09:50:14 -0300

    pracring: main|app-moduole v2

--- app-test/src/app.module.ts
+++ app-test/src/app.module.ts
@@ -1,38 +1,87 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ClassSerializerInterceptor, Logger, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { FeedModule } from './feed/feed.module';
-import { AuthModule } from './auth/auth.module';
-import { APP_FILTER } from '@nestjs/core';
-import { AllExceptionsFilter } from './core/all-exceptions.filter';
-import { ChatModule } from './chat/chat.module';
-
+import { HttpExceptionFilter } from './filters/http-exception.filter';
+import {
+  LoggerInterceptor,
+  RedisCacheInterceptor,
+  RedisLimitInterceptor,
+  ApiInterceptor,
+} from './interceptors';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
+import { ValidationPipe } from './pipe/validation.pipe';
+import { getConfig } from './utils';
+import { ApiModule } from './api/api.module';
+import { SharedModule } from './shared/shared.module';
+import { PluginModule } from './plugin/plugin.module';
+import { config } from 'process';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true}),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(<string>process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWROD,
-      database: process.env.POSTGRES_DATABASE,
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      ignoreEnvFile: false,
+      isGlobal: true, 
+      load: [getConfig],
     }),
-    FeedModule,
-    AuthModule,
-    ChatModule
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactoy: (ConfigService: ConfigService) => ({
+        type: 'mysql',
+        host: String(ConfigService.get('datasource.host')),
+        port: Number.parseInt(ConfigService.get('datasource.port') ?? '3306'),
+        username: String(ConfigService.get('datasource.username')),
+        password: String(ConfigService.get('datasource.password')),
+        database: [__dirname + '/**/*.entity{.ts,.js}'],
+        loggin: ConfigService.get('datasource.loggin'),
+        timezone: '+008:00',
+        cache: {
+          duration: 60000,
+        },
+        extra: {
+          pollMax: 32,
+          pollMin: 16,
+          queueTimeout: 60000,
+          pollPingInteval: 60,
+          pollTimeout: 60,
+        },
+      }),
+    }),
+    ApiModule,
+    SharedModule,
+    PluginModule,
+
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
+    Logger,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: RedisLimitInterceptor
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: RedisCacheInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
     {
       provide: APP_FILTER,
-      useClass: AllExceptionsFilter,
+      useClass: HttpExceptionFilter,
     }
   ],
 })
--- app-test/src/main.ts
+++ app-test/src/main.ts
@@ -1,24 +1,35 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-
-import * as fs from 'fs';
-import * as morgan from 'morgan';
-
+import { VersioningType } from '@nestjs/common/enums';
+import { Logger } from '@nestjs/common/services';
 import { AppModule } from './app.module';
+import { getConfig, IS_DEV } from './utils';
 
+export const config = getConfig();
+const PORT = config.PORT || 8080;
+const PREFIX = config.PREFIX || '/';
 
-const logStream = fs.createWriteStream('api.log', {
-  flags: 'a', // append
-});
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule)
+  const logger: Logger = new Logger('main.ts');
+
+  const app = await NestFactory.create(AppModule, {
+    logger: IS_DEV ? ['log', 'debug', 'error', 'warn'] : ['error', 'warn'],
+  });
+
   app.enableCors();
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe());
-  app.use(morgan('tiny', { stream: logStream }));
-  await app.listen(3000);
+
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI, 
+  });
+
+  app.setGlobalPrefix(PREFIX);
+  await app.listen(PORT, () => {
+    logger.log(`server running in the port ${PORT}/${PREFIX}`)
+  })
+
+
 
 
 }
